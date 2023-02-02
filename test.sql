f954e536eb2c0a0229e60172b009109d03653ed9
Deepikavin <116055801+Deepikavin@users.noreply.github.com>
2023-02-02 11:33:12 +0530

    Create test.sql

---
+++ test.sql
@@ -0,0 +1,13 @@
+create table persons (
+personID int,
+lastName varchar(50),
+FirstName varchar (50),
+city varchar(50)
+);
+
+insert into persons (
+personID, lastName, FirstName, city
+)
+values (
+1, 'rvin', 'Deepika', 'mysore'
+);
